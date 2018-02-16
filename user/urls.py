667089c97e2cdf036a146be360e257ca9675540b
Elisa <elisa.naskali@gmail.com>
2018-02-16 21:23:42 +0200

    User can change profile information

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^profiles/$', views.profiles, name='profiles'),
     url(r'^manage_games/$', views.manage_games, name='manage_games'),
+    url(r'^update_profile/$', views.update_profile, name='update_profile'),
 ]
