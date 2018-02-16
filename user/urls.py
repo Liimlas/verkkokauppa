c1592517fcd8e2784d79c0583e71e70e981f6acc
Elisa <elisa.naskali@gmail.com>
2018-02-16 18:15:35 +0200

    Now it works

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^profiles/$', views.profiles, name='profiles'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from main.models import Game
 
 @login_required
