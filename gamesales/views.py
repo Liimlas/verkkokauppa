88e184d9278f65c3e5ac829c60341bc3756d16e7
Sofia Kimpimäki <kimpims@aalto.fi>
2018-02-16 14:49:36 +0200

    submit button works

--- gamesales/views.py
+++ gamesales/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from main.models import Game
+
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+
+from django.http import HttpResponseRedirect
+
+
+
+#from .forms import AddGame
 
 
 def list_of_ids(gamelist):
@@ -26,5 +37,38 @@ def viewgame(request, id):
 
 
 
+#def addGame(request):
+ #   return render(request, 'gamesales/addGame.html')
+
+
+
+
+# disabling csrf (cross site request forgery)
+@csrf_exempt
 def addGame(request):
-    return render(request, 'gamesales/addGame.html')
+    # if post request came
+    if request.method == 'POST':
+        # getting values from post
+        developer = request.POST.get('developer')
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+
+        # adding the values in a context variable
+        context = {
+            'developer': developer,
+            'title': title,
+            'description': description,
+            'price': price
+        }
+
+        # getting our showdata template
+        template = loader.get_template('gamesales/showdata.html')
+
+        # returing the template
+        return HttpResponse(template.render(context, request))
+    else:
+        # if post request is not true
+        # returing the form template
+        template = loader.get_template('gamesales/addGame.html')
+        return HttpResponse(template.render())
