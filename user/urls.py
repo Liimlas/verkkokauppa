02b03be803ab589ba7462cc08baa26a389636586
Elisa <elisa.naskali@gmail.com>
2018-02-18 20:29:54 +0200

    Fixed few bugs, some are still around

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/([a-zA-Z0-9]+)/$', views.viewUser, name='user'),
     url(r'^profile/update_profile/(?P<pk>\d+)/$', views.update_profile, name='update_profile'),
     url(r'^upload/$', views.model_form_upload, name='upload'),
     url(r'^signup/$', views.signup, name='signup'),
