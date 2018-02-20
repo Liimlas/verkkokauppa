83930b306c6581e79734c172823fd725effc3af8
Sofia Kimpimäki <kimpims@aalto.fi>
2018-02-20 17:53:04 +0200

    new game style

--- user/urls.py
+++ user/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^upload/$', views.model_form_upload, name='upload'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^profiles/$', views.profiles, name='profiles'),
-    url(r'^manage_games/([a-zA-Z0-9]+)/$', views.manage_games, name='manage_games'),
-    url(r'^edit_games/([a-zA-Z0-9]+)/$', views.edit, name='manage_games'),
+    url(r'^manage_games/$', views.manage_games, name='manage_games'),
+    url(r'^manage_games/([a-zA-Z0-9]+)/$', views.edit, name='manage_games'),
 ]
