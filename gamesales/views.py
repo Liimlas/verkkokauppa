93a8fd6f2398f3becfe1c0320e68294decadd40d
Elisa <elisa.naskali@gmail.com>
2018-02-20 19:30:53 +0200

    Adding pictures works

--- gamesales/views.py
+++ gamesales/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Game, BoughtGame
+from .forms import AddGameForm
 import random
 from django.utils import timezone
-
-
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
-
-
-from django.http import HttpResponseRedirect
 
 
 def games(request):
@@ -38,7 +34,7 @@ def list_of_ids():
 
 
 #def addGame(request):
- #   return render(request, 'gamesales/addGame.html')
+#   return render(request, 'gamesales/addGame.html')
 def generate():
     letters = list("abcdefghijklmnopqrstuvwxyzABCDEFGHILJKLMNOPQRSTUVWXYZ0123456789")
     letter_list_size = len(letters)
@@ -59,57 +55,79 @@ def usePhoto():
     for photo in photoList:
         photos.append(photo.photoLink)
     return photos
-     # do it the way that photo is in the home page
+    # do it the way that photo is in the home page
 
 
-@csrf_exempt
 def addGame(request):
-    # if post request came
     if request.method == 'POST':
-        # getting values from post
-        gamename = request.POST.get('name')
-        gamedeveloper = request.POST.get('developer')
-        gameprice = request.POST.get('price')
-        gamelink = request.POST.get('link')
-        gamePhotoLink = request.POST.get('photoLink')
-        gameUsePhoto = request.POST.get('usePhoto')
+        form = AddGameForm(request.POST, request.FILES)
+        context= {}
 
+        if form.is_valid():
+            newGame = form.save(commit=False)
+            newGame.developer = request.user
+            newGame.saleprice = 0
+            newGame.onsale = False
+            newGame.soldcopies = 0
+            newGame.publish_date = timezone.now()
 
-        if len(gamename) < 3 or len(gamename) > 60:
-            context = {}
-            context['nameError'] = True
-            return render(request, 'gamesales/addGame.html', context)
-            return render(request, 'gamesales/addGame.html', context)
-        elif gamelink.find('.') == -1 and gamelink.find('www') == -1:
-            context = {}
-            context['linkError'] = True
-            return render(request, 'gamesales/addGame.html', context)
-        else:
-            gameid = generate()
-            context = {
-                'name': gamename,
-                'price': gameprice,
-                'link': gamelink,
-                'id' : gameid,
-                'photoLink': gamePhotoLink
-            }
+            newID = generate()
+            newGame.id = newID
+            newGame.save()
 
-            a = Game.objects.create(developer=request.user, name=gamename, id=gameid, price=gameprice, saleprice=1, onsale=False, soldcopies=0, link=gamelink, publish_date=timezone.now())
-            b = BoughtGame.objects.create(owner=request.user, game=a)
-            a.save()
-            b.save()
-
-
-        # getting our showdata template
-        return render(request, 'gamesales/showdata.html', context)
+            return redirect('index')
     else:
-        # if post request is not true
-        # returing the form template
-        return render(request, 'gamesales/addGame.html')
+        form = AddGameForm()
+
+
+    return render(request, 'gamesales/addGame.html', {'form': form})
+
+# @csrf_exempt
+# def addGame(request):
+#     # if post request came
+#     if request.method == 'POST':
+#         # getting values from post
+#         gamename = request.POST.get('name')
+#         gamedeveloper = request.POST.get('developer')
+#         gameprice = request.POST.get('price')
+#         gamelink = request.POST.get('link')
+#         gamePhotoLink = request.POST.get('photoLink')
+#         gameUsePhoto = request.POST.get('usePhoto')
+
+
+#         if len(gamename) < 3 or len(gamename) > 60:
+#             context = {}
+#             context['nameError'] = True
+#             return render(request, 'gamesales/addGame.html', context)
+#             return render(request, 'gamesales/addGame.html', context)
+#         elif gamelink.find('.') == -1 and gamelink.find('www') == -1:
+#             context = {}
+#             context['linkError'] = True
+#             return render(request, 'gamesales/addGame.html', context)
+#         else:
+#             gameid = generate()
+#             context = {
+#                 'name': gamename,
+#                 'price': gameprice,
+#                 'link': gamelink,
+#                 'id' : gameid,
+#                 'photoLink': gamePhotoLink
+#             }
+
+#             a = Game.objects.create(developer=request.user, name=gamename, id=gameid, price=gameprice, saleprice=1, onsale=False, soldcopies=0, link=gamelink, publish_date=timezone.now())
+#             b = BoughtGame.objects.create(owner=request.user, game=a)
+#             a.save()
+#             b.save()
+
+
+#         # getting our showdata template
+#         return render(request, 'gamesales/showdata.html', context)
+#     else:
+#         # if post request is not true
+#         # returing the form template
+#         return render(request, 'gamesales/addGame.html')
 
 def onSale(request):
     games = Game.objects.all()
     context = {'games': games}
     return render(request, 'gamesales/gameonsale.html', context)
-
-
